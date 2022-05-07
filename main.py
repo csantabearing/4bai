e17ea060779c804e50f63476c2e0a0611f94158b
csantabearing <camilo@bearing.ai>
2022-05-07 08:57:43 -0500

    Huggingface

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+sentiment_model = pipeline("sentiment-analysis")
+
+class PredictionRequest(BaseModel):
+  query_string: str
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World"} 
+
+@app.post("/sentiment")
+def sentiment(request: PredictionRequest):
+    sentiment = sentiment_model(request.query_string)
+    return {"sentiment": sentiment}
